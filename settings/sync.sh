79411247e0150dd8c66e95b0391eb3c41483b3ca
schneems <richard.schneeman+foo@gmail.com>
2018-09-10 09:34:50 -0500

    Default git branch pushes to current branch

--- settings/sync.sh
+++ settings/sync.sh
@@ -15,6 +15,7 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+git config --global push.default current
 
 echo "== Settup git"
 echo "git config user.name"
